b51e54663f2405b026df9bccc4e387e5b830bbac
j-w-s <69388380+j-w-s@users.noreply.github.com>
2021-04-15 15:03:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # IMPORTS
 ##################################
 
-import Piece, Board # Movement, Player
+import #pieces, board, movement, player
 from tkinter import *
 
 ##################################
